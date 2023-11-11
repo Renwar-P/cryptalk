ffcd0f1dfa2328fcb1e22a45e15b6a489a73d4f8
Renwar-P <>
2023-11-11 11:08:38 +0000

    installed crispy and created class CommentForm

--- cryptalk/settings.py
+++ cryptalk/settings.py
@@ -47,13 +47,17 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django_summernote',
+    'crispy_forms',
     'blog',
+
 ]
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
