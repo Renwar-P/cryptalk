6e80b7df694880e9a85c0d6118200b5f4a9900d1
Renwar-P <>
2023-11-17 19:24:09 +0000

    add edit class for edit/delete links and btn-lg to footer

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+    content = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
