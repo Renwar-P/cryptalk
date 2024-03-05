f8ac46a41ccf5715c4ed13661ec96873818c45ec
Renwar-P <renwar_86@hotmail.com>
2024-03-05 22:13:38 +0000

    add max_cap to CoinType

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class CoinType(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
+    max_cap = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
